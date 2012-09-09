15d26496a8e9f02338ed6d86ce4761012fa12abf
Stas SUȘCOV <stas@net.utcluj.ro>
2012-09-09 19:07:21 +0300

    Added support for adding and removing options.

--- src/quiz_builder.js
+++ src/quiz_builder.js
@@ -50,12 +50,44 @@
       $( self.element ).append( tmpl );
     },
 
+    removeAnswer: function( event ) {
+      var self = event.data;
+      if ( $(this).parents( 'p' ).size() > 0 ) {
+        $(this).parent( 'p' ).remove();
+      } else {
+        return;
+      }
+    },
+
+    duplicateAnswer: function( event ) {
+      var self = event.data,
+          parent = $(this).parent().find( 'p' ).clone();
+
+      if ( !!parent[0] ) {
+        parent = parent[0];
+        $(parent).find( 'input' ).val( '' );
+        $(this).after(parent);
+      } else {
+        return;
+      }
+    },
+
     controls: function() {
       var self = this;
+
+      // Bind main controls
       $.each( this.options.controls, function( key, ctrl ){
         $( ctrl ).on( 'click', self, self[ key + 'Handler' ] );
-      })
-    },
+      });
+
+      // Bind radio/checkboxes deletion controls
+      $.each( this.options.templates, function( key, ctrl ){
+        var toRem = ctrl.replace( '.template', ' p .remove' );
+        var toDup = ctrl.replace( '.template', ' .duplicate' );
+        $( self.element ).on( 'click', toRem, self, self[ 'removeAnswer' ] );
+        $( self.element ).on( 'click', toDup, self, self[ 'duplicateAnswer' ] );
+      });
+    }
   }
 
   // A really lightweight plugin wrapper around the constructor,
