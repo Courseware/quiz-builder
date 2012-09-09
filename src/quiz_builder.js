5bb3de75738e392b2114b1e2e3e7d9fdb79ca6a7
Stas SUȘCOV <stas@net.utcluj.ro>
2012-09-09 18:12:35 +0300

    Added basic structure of the plugin.

--- src/quiz_builder.js
+++ src/quiz_builder.js
@@ -1,1 +1,78 @@
-var QuizBuilder = {};
+;(function ( $, window, undefined ) {
+  var pluginName = 'quizBuilder',
+      document = window.document,
+      defaults = {
+        controls: {
+          text: '.quiz-new-text',
+          radios: '.quiz-new-radios',
+          checkboxes: '.quiz-new-checkboxes'
+        },
+        templates: {
+          text: '.quiz-text.template',
+          radios: '.quiz-radios.template',
+          checkboxes: '.quiz-checkboxes.template'
+        }
+      };
+
+  // Plugin Constructor
+  function QuizBuilder( element, options ) {
+    this.element = element;
+
+    this.options = $.extend( {}, defaults, options) ;
+
+    this._defaults = defaults;
+    this._name = pluginName;
+
+    this.controls();
+  }
+
+  QuizBuilder.prototype = {
+    constructor: QuizBuilder,
+
+    textHandler: function( event ) {
+      var self = event.data;
+      var tmpl = $( self.options.templates.text ).clone();
+      tmpl.removeClass( 'template' ).show();
+      $( self.element ).append( tmpl );
+    },
+
+    radiosHandler: function( event ) {
+      var self = event.data;
+      var tmpl = $( self.options.templates.radios ).clone();
+      tmpl.removeClass( 'template' ).show();
+      $( self.element ).append( tmpl );
+    },
+
+    checkboxesHandler: function( event ) {
+      var self = event.data;
+      var tmpl = $( self.options.templates.checkboxes ).clone();
+      tmpl.removeClass( 'template' ).show();
+      $( self.element ).append( tmpl );
+    },
+
+    controls: function() {
+      var self = this;
+      $.each( this.options.controls, function( key, ctrl ){
+        $( ctrl ).on( 'click', self, self[ key + 'Handler' ] );
+      })
+    },
+  }
+
+  // A really lightweight plugin wrapper around the constructor,
+  // preventing against multiple instantiations
+  $.fn[pluginName] = function ( options ) {
+    return this.each(function () {
+      if ( !$.data( this, 'quiz-options' ) ) {
+        $.data( this, 'quiz-options', new QuizBuilder( this, options ) );
+      }
+    });
+  }
+
+  $(window).on('load', function () {
+    $('[data-quiz="auto"]').each(function () {
+      var $quiz = $(this);
+      $quiz.quizBuilder($quiz.data());
+    })
+  })
+
+}(jQuery, window));
