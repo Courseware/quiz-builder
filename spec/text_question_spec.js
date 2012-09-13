e5f092971317537b279b922512f9df940e11c41f
Stas SUȘCOV <stas@net.utcluj.ro>
2012-09-13 18:06:38 +0300

    Added jasmine runner and a simple spec.

---
+++ spec/text_question_spec.js
@@ -0,0 +1,9 @@
+describe( 'Quiz Builder text button', function() {
+  it( 'should exist', function() {
+    expect(true).toBe(true);
+  });
+
+  it( 'should exist', function() {
+    expect(true).toBe(false);
+  });
+});
